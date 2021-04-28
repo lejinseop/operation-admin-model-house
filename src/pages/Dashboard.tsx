d09f775d19c964f6892427b9877792e5e3aff656
gyuusik <gs.kim@birdview.kr>
2021-04-28 16:51:04 +0900

    chore: 문의 목록 출력을 위해 대시보드 구조를 재정의하라

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,7 +1,14 @@
 import type { FC } from 'react'
 
+import InquiryList from '~/features/inquiry/InquiryList'
+
 const Dashboard: FC = () => {
-  return <div>dashboard</div>
+  return (
+    <div>
+      <h2>dashboard</h2>
+      <InquiryList />
+    </div>
+  )
 }
 
 export default Dashboard
